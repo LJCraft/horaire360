9c7feeeb96708481d53b8e460e09aaef1e3f3c9a
LJCraft <jordan.liale@facsciences-uy1.cm>
2025-05-11 07:04:17 +0200

    photo upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -311,5 +311,8 @@ use Illuminate\Support\Facades\Route;
     </div>
     
     @stack('scripts')
+    
+    <!-- Scripts Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
